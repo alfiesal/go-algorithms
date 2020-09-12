bf1af0294c89d3890af0d10f1890456ced08d690
Alfie Salomons <salomonsalfie@gmail.com>
2020-09-12 23:26:34 +0200

    :sparkles: Add iterative binary search

--- search/binary_search.go
+++ search/binary_search.go
@@ -2,6 +2,7 @@ package search
 
 import "fmt"
 
+// Recursive binary search algorithms
 func binarySearch(input []int, query int, lowIndex int, highIndex int) int {
 	if highIndex < lowIndex {
 		return -1
@@ -24,6 +25,29 @@ func binarySearch(input []int, query int, lowIndex int, highIndex int) int {
 	return -1
 }
 
+// Iterative binary search algorithms
+func iterativeBinarySearch(input []int, query int) int {
+	lowIndex := 0
+	highIndex := len(input)
+
+	for lowIndex < highIndex {
+		middleIndex := int(lowIndex + (highIndex-lowIndex)/2)
+		if query == input[middleIndex] {
+			return middleIndex
+		}
+
+		if query < input[middleIndex] {
+			highIndex = middleIndex - 1
+		}
+
+		if query > input[middleIndex] {
+			lowIndex = middleIndex + 1
+		}
+	}
+
+	return -1
+}
+
 // Example of usage binary search algorithm
 func BinarySearchExample() {
 	fmt.Println("####################################")
@@ -32,14 +56,28 @@ func BinarySearchExample() {
 
 	input := []int{1, 2, 3, 4, 8, 10, 11, 20, 21, 24, 29, 30}
 	query := 11
-	index := binarySearch(input, query, 0, len(input))
 
-	if index == -1 {
+	fmt.Println("Recursive")
+	recursiveIndex := binarySearch(input, query, 0, len(input))
+
+	if recursiveIndex == -1 {
 		fmt.Println("Can't find element: ", query)
 
 		return
 	}
 
-	fmt.Println("Index of the found element: ", index)
-	fmt.Println("Value of found element: ", input[index])
+	fmt.Println("Index of the found element: ", recursiveIndex)
+	fmt.Println("Value of found element: ", input[recursiveIndex])
+
+	fmt.Println("Iterative")
+	iterativeIndex := iterativeBinarySearch(input, query)
+
+	if iterativeIndex == -1 {
+		fmt.Println("Can't find element: ", query)
+
+		return
+	}
+
+	fmt.Println("Index of the found element: ", iterativeIndex)
+	fmt.Println("Value of found element: ", input[iterativeIndex])
 }
